ba9ecc71ddee9fd8599207712d3fa12296f3675d
Maria Lobillo <maria.lobillo.santos@gmail.com>
2018-05-06 13:44:33 +0200

    refactoring

--- sum.test.js
+++ sum.test.js
@@ -52,4 +52,11 @@ test('User fetched name should be Leanne Graham', () => {
 		.then(data => {
 			expect(data.name).toEqual('Leanne Graham');
 		});
-})
+});
+
+// Async Await
+test('User fetched name should be Leanne Graham - async await', async () => {
+	expect.assertions(1);
+	const data = await functions.fetchUser();
+	expect(data.name).toEqual('Leanne Graham');
+});
